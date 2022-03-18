ff7762e4585f192f7e46d30ed3a6eaa0953a58f4
happenwah <tiagolixa@hotmail.com>
2022-03-18 11:57:52 +0000

    chore: bump metabox-sdk

--- packages/metabox-sdk/src/constants/index.ts
+++ packages/metabox-sdk/src/constants/index.ts
@@ -11,6 +11,8 @@ const getMetaBoxAddress = (chainId: number): string => {
   switch (chainId) {
     case 5:
       return utils.getAddress("0x6be602e1E1629789E64bf30d56139396b232597C");
+    case 137:
+      return utils.getAddress("0xbeC333EDE1A0687D2b9624F8C073a54c93ba9777");
     case 80001:
       return utils.getAddress("0xeeea839E2435873adA11d5dD4CAE6032742C0445");
     default:
